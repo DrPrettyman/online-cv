ca3f95e5c95be07fd578789a6c05a45fa418a8f7
Joshua Prettyman <j.prettyman@icloud.com>
2024-11-29 22:09:42 +0100

    update images

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ const InteractiveDashboard: React.FC = () => {
           
           <div className="rounded-lg overflow-hidden shadow-2xl mb-6 w-full">
             <img 
-              src="/online-cv/images/headshot.jpg" 
+              src="/online-cv/images/headshot.jpeg" 
               alt="Joshua Prettyman looking handsome" 
               className="object-cover w-full h-full"
             />
